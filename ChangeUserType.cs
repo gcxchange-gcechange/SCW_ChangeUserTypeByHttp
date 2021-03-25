73c0ae7c0ef47d2dd5c9d43c5abbe2c63ab7c63c
Darren Kao <dkao@tbssctdev.onmicrosoft.com>
2021-03-25 16:20:43 -0400

    Add project files.

---
+++ ChangeUserType.cs
@@ -0,0 +1,146 @@
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using System.Configuration;
+using System;
+using System.Text;
+using System.Web;
+using System.IO;
+using System.Web.Script.Serialization;
+using Microsoft.Graph;
+using System.Net.Http.Headers;
+
+namespace ChangeUserTypeByHttp
+{
+    public static class ChangeUserType
+    {
+        [FunctionName("ChangeUserType")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequestMessage req, TraceWriter log)
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+
+            // parse query parameter
+            string name = req.GetQueryNameValuePairs()
+                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
+                .Value;
+
+            if (name == null)
+            {
+                // Get request body
+                dynamic data = await req.Content.ReadAsAsync<object>();
+                name = data?.name;
+            }
+
+            log.Info(name);
+
+            var authResult = GetOneAccessToken();
+            var graphClient = GetGraphClient(authResult);
+
+            ChangeGuestUserType(graphClient, log, name);
+
+            return req.CreateResponse(HttpStatusCode.OK, "Finished. ");
+
+        }
+
+        public static string GetOneAccessToken()
+        {
+            string token = "";
+            string CLIENT_ID = ConfigurationManager.AppSettings["CLIENT_ID"];
+            string CLIENT_SECERET = ConfigurationManager.AppSettings["CLIENT_SECRET"];
+            string TENAT_ID = "ddbd240e-11ba-47a6-abeb-e1a6be847a17";
+            string TOKEN_ENDPOINT = "";
+            string MS_GRAPH_SCOPE = "";
+            string GRANT_TYPE = "";
+
+            try
+            {
+
+                TOKEN_ENDPOINT = "https://login.microsoftonline.com/" + TENAT_ID + "/oauth2/v2.0/token";
+                MS_GRAPH_SCOPE = "https://graph.microsoft.com/.default";
+                GRANT_TYPE = "client_credentials";
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("A error happened while search config file");
+            }
+            try
+            {
+                HttpWebRequest request = WebRequest.Create(TOKEN_ENDPOINT) as HttpWebRequest;
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+                StringBuilder data = new StringBuilder();
+                data.Append("client_id=" + HttpUtility.UrlEncode(CLIENT_ID));
+                data.Append("&scope=" + HttpUtility.UrlEncode(MS_GRAPH_SCOPE));
+                data.Append("&client_secret=" + HttpUtility.UrlEncode(CLIENT_SECERET));
+                data.Append("&GRANT_TYPE=" + HttpUtility.UrlEncode(GRANT_TYPE));
+
+                byte[] byteData = UTF8Encoding.UTF8.GetBytes(data.ToString());
+                request.ContentLength = byteData.Length;
+                using (Stream postStream = request.GetRequestStream())
+                {
+                    postStream.Write(byteData, 0, byteData.Length);
+                }
+
+                // Get response
+
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+
+                    using (var reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        JavaScriptSerializer js = new JavaScriptSerializer();
+                        var objText = reader.ReadToEnd();
+                        LgObject myojb = (LgObject)js.Deserialize(objText, typeof(LgObject));
+                        token = myojb.access_token;
+                    }
+
+                }
+                return token;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("A error happened while connect to server please check config file");
+                return "error";
+            }
+        }
+
+        public static GraphServiceClient GetGraphClient(string authResult)
+        {
+            GraphServiceClient graphClient = new GraphServiceClient(
+                 new DelegateAuthenticationProvider(
+            async (requestMessage) =>
+            {
+                requestMessage.Headers.Authorization =
+                    new AuthenticationHeaderValue("bearer",
+                    authResult);
+            }));
+            return graphClient;
+        }
+
+        public static async void ChangeGuestUserType(GraphServiceClient graphClient, TraceWriter Log, string userIdOrEmail)
+        {
+            var guestUser = new User
+            {
+                UserType = "Member"
+            };
+            try
+            {
+                await graphClient.Users[userIdOrEmail]  //"6c3520af-ddd8-4f77-b18d-44e70b88f4d9"
+                .Request()
+                .UpdateAsync(guestUser);
+                Log.Info($"Change {userIdOrEmail} user type to member successfully");
+            }
+            catch (Exception ex)
+            {
+                Log.Info($"error message: {ex.Message}");
+            }
+
+        }
+
+    }
+}
