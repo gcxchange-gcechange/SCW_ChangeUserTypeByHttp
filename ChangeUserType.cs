5c9ec40a0179ec0d67dab1ca85f0a19e9ba68b1d
Stéphanie Lefebvre <STLEFEBV@tbs-sct.gc.ca>
2021-05-05 18:07:41 +0000

    Updated ChangeUserType.cs

--- ChangeUserType.cs
+++ ChangeUserType.cs
@@ -43,7 +43,6 @@ namespace ChangeUserTypeByHttp
             ChangeGuestUserType(graphClient, log, name);
 
             return req.CreateResponse(HttpStatusCode.OK, "Finished. ");
-
         }
 
         public static string GetOneAccessToken()
